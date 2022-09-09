a4ac111fbbecb3b4fe91754f9863931f672463cf
Daniel Ogbonna <mercyland811@gmail.com>
2022-09-09 04:54:24 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,28 +1,43 @@
-#include <stdio.h>                                                                                                                                                        
+#include <stdio.h>
 
-                                                                                                                                                                          
 
-/**                                                                                                                                                                       
 
-* main - print the string in the put function                                                                                                                             
+/**
 
-*                                                                                                                                                                         
+  * main - Prints the sizeof many var types
 
-* Description: using the main function
-* this program prints the size of various types on the computer it is compileed on
-* Return: 0
-*/
+  *
+
+  * Return: Always 0 (Success)
+
+  */
+
 int main(void)
-{
-        char c;
-        int i;
-        long lli;
-        float f;
 
-        printf("Size of a char: %ld byte(s)\n", sizeof(c));
-        printf("Size of an int: %ld byte(s)\n", sizeof(i));
-        printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-        printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-        printf("Size of a  float: %ld byte(s)\n", sizeof(f));
-        return (0);
+{
+
+	char a;
+
+	int b;
+
+	long int c;
+
+	long long int d;
+
+	float e;
+
+
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+
+	return (0);
+
 }
